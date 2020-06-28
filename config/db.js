66ffb6df9b28060047c5bad478c9134d9f899692
ppalone <pranjalalone@gmail.com>
2020-06-28 23:59:12 +0530

    Setup new database & changes in package.json

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const URI = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@anonymous-blog-eqhl0.mongodb.net/<dbname>?retryWrites=true&w=majority`
+const URI = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@crypt.eqhl0.mongodb.net/<dbname>?retryWrites=true&w=majority`
 
 mongoose.connect(URI, {
     useUnifiedTopology: true,
