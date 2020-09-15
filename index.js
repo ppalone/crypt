7255876494778729534df4bfbb297b232dd2b923
ppalone <pranjalalone@gmail.com>
2020-09-15 16:28:34 +0530

    Bug fixes and improved user experience

--- index.js
+++ index.js
@@ -9,9 +9,13 @@ const methodOverride = require('method-override');
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+// Middlewares
+const forwardAuthenticated = require('./middlewares/auth').forwardAuthenticated;
+
 // Routes Handlers
 const usersHandler = require('./routes/users');
 const blogsHandler = require('./routes/blogs');
+const profileHandler = require('./routes/profile');
 const verificationHandler = require('./routes/verify');
 
 // Mongoose Config
@@ -34,11 +38,11 @@ app.locals.moment = require('moment');
 
 // Express Sessions
 app.use(
-	session({
-		secret: 'code',
-		resave: true,
-		saveUninitialized: true,
-	})
+  session({
+    secret: 'code',
+    resave: true,
+    saveUninitialized: true,
+  })
 );
 
 // Passport
@@ -50,31 +54,32 @@ app.use(flash());
 
 // Method override
 app.use(
-	methodOverride(function (req, res) {
-		if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-			// look in urlencoded POST bodies and delete it
-			var method = req.body._method;
-			delete req.body._method;
-			return method;
-		}
-	})
+  methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+      // look in urlencoded POST bodies and delete it
+      var method = req.body._method;
+      delete req.body._method;
+      return method;
+    }
+  })
 );
 
 // Global Variables
 app.use((req, res, next) => {
-	res.locals.success_msg = req.flash('success_msg');
-	res.locals.error_msg = req.flash('error_msg');
-	res.locals.error = req.flash('error');
-	next();
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
 });
 
-app.get('/', (req, res) => res.render('index.ejs'));
+app.get('/', forwardAuthenticated, (req, res) => res.render('index.ejs'));
 app.use('/users', usersHandler);
 app.use('/blogs', blogsHandler);
+app.use(profileHandler);
 app.use(verificationHandler);
 
 app.get('*', (req, res) => {
-	res.render('errors/404');
+  res.render('errors/404');
 });
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,10 +1,13 @@
 module.exports = {
-	ensureAuthenticated: (req, res, next) => {
-		if (!req.isAuthenticated()) return res.redirect('/users/login');
-		next();
-	},
-	forwardAuthenticated: (req, res, next) => {
-		if (req.isAuthenticated()) return res.redirect('/blogs');
-		next();
-	},
+  ensureAuthenticated: (req, res, next) => {
+    if (!req.isAuthenticated()) {
+      req.flash('error_msg', 'You must be logged in to view this resource');
+      return res.redirect('/users/login');
+    }
+    next();
+  },
+  forwardAuthenticated: (req, res, next) => {
+    if (req.isAuthenticated()) return res.redirect('/blogs');
+    next();
+  },
 };
