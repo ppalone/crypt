3b9c6421ab07cd407f5296a7ed40c2c62953fe58
ppalone <pranjalalone@gmail.com>
2020-09-21 08:15:06 +0530

    Added a profile and change password page

--- index.js
+++ index.js
@@ -24,8 +24,6 @@ require('./config/db');
 // Passport
 require('./config/passport')(passport);
 
-// Error Resolved the app wasn't authenticating because I didn't add body parser middleware
-// Body Parser Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -56,7 +54,6 @@ app.use(flash());
 app.use(
   methodOverride(function (req, res) {
     if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-      // look in urlencoded POST bodies and delete it
       let method = req.body._method;
       delete req.body._method;
       return method;
--- routes/profile.js
+++ routes/profile.js
@@ -1,11 +1,23 @@
 const router = require('express').Router();
+const User = require('../models/User');
 
 const authMiddleware = require('../middlewares/auth');
 
 // router.use(authMiddleware.ensureAuthenticated);
 
-router.get('/profile', authMiddleware.ensureAuthenticated, (req, res) => {
-  res.render('profile/profile.ejs');
+router.get('/profile', authMiddleware.ensureAuthenticated, async (req, res) => {
+  // Get the current logged in user
+  try {
+    let user = await User.findById({ _id: req.user._id });
+    res.render('profile/profile.ejs', { user });
+  } catch (err) {
+    res.send('Server internal error');
+    console.log(err);
+  }
+});
+
+router.get('/changepassword', authMiddleware.ensureAuthenticated, (req, res) => {
+  res.render('profile/changepassword');
 });
 
 module.exports = router;
