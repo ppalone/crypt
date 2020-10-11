032dd0997042cd3fa990429081e9f3beb48c89b0
ppalone <pranjalalone@gmail.com>
2020-10-11 17:20:16 +0530

    Remove secure cookie option

--- index.js
+++ index.js
@@ -45,12 +45,6 @@ app.use(
     secret: process.env.SESSION_SECRET,
     resave: true,
     saveUninitialized: true,
-    cookie: {
-      maxAge: 1000 * 60 * 60 * 24 * 30,
-      httpOnly: true,
-      sameSite: true,
-      secure: process.env.NODE_ENV === 'production',
-    },
   }),
 );
 
